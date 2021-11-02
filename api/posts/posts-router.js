3cb42b06b59e8e792f8b61eafdfe5a3a70c4223a
Craig Barcinas <cbarcinas@live.com>
2021-11-01 21:06:32 -0700

    get req findByID working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,17 +3,47 @@
 const express = require("express");
 const router = express.Router();
 
+// import data access functions
+const Post = require("./posts-model");
+
 // ENDPOINTS
 // ENDPOINTS
 // ENDPOINTS
 
 // Returns an array of all the post objects contained in the database
 router.get("/", (req, res) => {
-  res.json("router is connected");
+  Post.find()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
 });
 
-// // Returns the post object with the specified id
-// router.get("/:id", (req, res) => {});
+// Returns the post object with the specified id
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(404).json({
+      message: "The post information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
 
 // // Creates a post using the info sent inside the req body and returns the newly created post object
 // router.post("/", (req, res) => {});
